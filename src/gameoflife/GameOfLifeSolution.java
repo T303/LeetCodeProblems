4edfde347dea2680ac8fb32cdc91e79e07d6fb80
Tim <44584257+T303@users.noreply.github.com>
2022-04-12 17:55:18 -0500

    Isolated current Solution

--- src/gameoflife/GameOfLifeSolution.java
+++ src/gameoflife/GameOfLifeSolution.java
@@ -3,6 +3,54 @@ package gameoflife;
 public class GameOfLifeSolution {
 	int[] possibleNeighborsX = {-1, -1, 0, 1, 1, 1, 0, -1};
 	int[] possibleNeighborsY = {0, -1, -1, -1, 0, 1, 1, 1};
+	
+	public void gameOfLifeTest(int[][] board) {
+		int[][] boardCopy = new int[board.length][board[0].length];
+		
+		for(int i = 0; i < board.length; i++) {
+			for(int j = 0; j < board[i].length; j++) {
+				boardCopy[i][j] = board[i][j];
+			}
+		}
+		
+		
+		for(int i = 0; i < boardCopy.length; i++) {
+			for(int j = 0; j < boardCopy[i].length; j++) {
+				int livingNeighbors = 0;
+				for(int k = 0; k < 8; k++) {
+					try {
+						int neighborValue = boardCopy[i + possibleNeighborsX[k]][j + possibleNeighborsY[k]];
+						if(neighborValue == 1) {
+							livingNeighbors++;
+						}
+					} catch(ArrayIndexOutOfBoundsException e) {
+						continue;
+					}
+				}
+				if(boardCopy[i][j] == 1) {
+					if(livingNeighbors < 2) {
+						board[i][j] = 0;
+					}
+					if(livingNeighbors == 2 || livingNeighbors == 3) {
+						board[i][j] = 1;
+					}
+					if(livingNeighbors > 3) {
+						board[i][j] = 0;
+					}
+				}
+				if(boardCopy[i][j] == 0) {
+					if(livingNeighbors == 3) {
+						board[i][j] = 1;
+					}
+				}
+				
+			}
+		}
+    }
+	
+	/*
+	 * This was the original method that I wrote that returned a copy of the board matrix
+	 * I had to modify it as Leetcode expects modifying the board directly
 	public int[][] gameOfLife(int[][] board) {
 		int[][] nextState = new int[board.length][board[0].length];
 		
@@ -40,48 +88,5 @@ public class GameOfLifeSolution {
 		}
 		return nextState;
 	}
-	
-	public void gameOfLifeTest(int[][] board) {
-        //int[][] nextState = new int[board.length][board[0].length];
-		int[][] boardCopy = new int[board.length][board[0].length];
-		
-		for(int i = 0; i < board.length; i++) {
-			for(int j = 0; j < board[i].length; j++) {
-				boardCopy[i][j] = board[i][j];
-			}
-		}
-		
-		for(int i = 0; i < boardCopy.length; i++) {
-			for(int j = 0; j < boardCopy[i].length; j++) {
-				int livingNeighbors = 0;
-				for(int k = 0; k < 8; k++) {
-					try {
-						int neighborValue = boardCopy[i + possibleNeighborsX[k]][j + possibleNeighborsY[k]];
-						if(neighborValue == 1) {
-							livingNeighbors++;
-						}
-					} catch(ArrayIndexOutOfBoundsException e) {
-						continue;
-					}
-				}
-				if(boardCopy[i][j] == 1) {
-					if(livingNeighbors < 2) {
-						board[i][j] = 0;
-					}
-					if(livingNeighbors == 2 || livingNeighbors == 3) {
-						board[i][j] = 1;
-					}
-					if(livingNeighbors > 3) {
-						board[i][j] = 0;
-					}
-				}
-				if(boardCopy[i][j] == 0) {
-					if(livingNeighbors == 3) {
-						board[i][j] = 1;
-					}
-				}
-				
-			}
-		}
-    }
+	*/
 }
